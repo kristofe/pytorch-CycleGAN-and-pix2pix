ebd2a52c8e02a222b7841b36304d0ce6b8216e87
Kristofer Schlachter <kristofe72@gmail.com>
2019-02-10 17:58:40 -0500

    Changed command line to have unet_256 as generator model

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,1 +1,3 @@
-python train.py --dataroot ./datasets/terrain_2560_small/aligned/ --name terrain_cyclegan --model cycle_gan --pool_size 50 --align_data
+#python train.py --dataroot ./datasets/terrain_2560_small/aligned/ --name terrain_cyclegan --model cycle_gan --pool_size 50 --align_data
+
+python train.py --dataroot ./datasets_demo/256/curr_topo_3k --name terrain_cyclegan_curr_topo --which_model_netG unet_256 --model cycle_gan --pool_size 50 --gpu_ids 1
